1f46f9cbb7a0439213116f43f2a2b093fbf3cba0
baperamerop <bpo.amerop@gmail.com>
2026-07-29 17:01:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.MONITORING_ANGGARAN_CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbwJazU3PZSzyiVHJqd8t-eAOL5_Pk7tZpcJ9R-W8ABmKIPdyRNNNOExOv9NuOwxTySnwQ/exec",
+
+  googleClientId: ""
+};
